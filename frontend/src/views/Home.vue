da308d501353193857d4ae227c204fddd90239e9
AlikBook <ivanarturograndi6@gmail.com>
2025-06-24 18:42:11 +0200

    correction

--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -83,7 +83,7 @@ const fetchEvents = async () => {
 };
 
 onMounted(async () => {
-  alert("wait 5 seconds for the databse to connect")
+  alert("wait 5 seconds for the database to connect")
   await delay(5000);
   await fetchTopGames();             
   await fetchEvents();         
